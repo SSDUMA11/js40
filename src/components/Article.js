5d0f8f291f97660643764f9a2a3a66a66e7e40af
SSDUMA11 <dumasofia2@gmail.com>
2023-04-03 16:26:27 +0300

    task 5

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,28 +1,16 @@
 import React from "react";
-
-function Button(props) {
-    return <button>{props.name}</button>;
-}
+import ArticleBody from "./ArticleBody";
+import ArticleActions from "./ArticleActions";
+import ArticleAuthor from "./ArticleAuthor"
 
 function Article() {
     return (
         <div>
             <div className="article__title">
                 <h2>NVIDIA Accelerated AI on Azure</h2>
-            </div> 
-            <div className="article__text">
-                <h3>Article description:</h3>
-                <p>NVIDIA on Azure is bringing AI, networking, and high-performanse computing to the enterprise</p>
-            </div>
-            <div className="article__button">
-                <Button name="Read" />
-                <Button name="Mark as read" />
-                <Button name="Mark as unread" />
-            </div>
-            <div className="article__autor">
-                <p>Author: Mike</p>
-                <p>Published: 06.12.2022</p>
-                <p>Theme: Video cards</p>
+                <ArticleBody />
+                <ArticleActions />
+                <ArticleAuthor />
             </div> 
         </div> 
     );
